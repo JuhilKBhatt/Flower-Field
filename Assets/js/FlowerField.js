eb27d7d0d7bd17d24257e4365e3664a5db28824f
juhilxbhatt <juhil.k.bhatt@student.uts.edu.au>
2025-03-01 23:18:16 +1100

    Update FlowerField.js

--- Assets/js/FlowerField.js
+++ Assets/js/FlowerField.js
@@ -2,7 +2,7 @@ import * as THREE from './build/three.module.js';
 import { CreateFlowerHead } from './FlowerHead.js';
 
 export function CreateFlowerField(scene) {
-    const flowerMaterial = new THREE.MeshStandardMaterial({ color: 0x00FF00 }); // Colour: Pink
+    const flowerMaterial = new THREE.MeshStandardMaterial({ color: 0x00FF00 }); // Colour: Green
     for (let i = 0; i < 100; i++) {
         const flower = new THREE.Mesh(
             new THREE.CylinderGeometry(0.1, 0.1, 1, 6),
